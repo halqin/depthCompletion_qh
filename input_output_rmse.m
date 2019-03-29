29cffca977025e5f8e16357f7c443a10580cf30a
halqin <gavin.qinhao@gmail.com>
2019-03-29 22:08:21 +0100

    in_out

--- input_output_rmse.m
+++ input_output_rmse.m
@@ -8,40 +8,28 @@ load(input_name);
 net = Net(net);
 size_ = size(imdb.images.data);
 % val_im = size_(4);
-val_im = 3;
-% imdb.images.data(:,:,4,:) = imdb.images.data(:,:,4,:)/80;
+val_im = 100;
+imdb.images.data(:,:,4,:) = imdb.images.data(:,:,4,:)/80;
 imdb.images.data(:,:,1:3,:) = imdb.images.data(:,:,1:3,:)/255;
 error =0; 
-ave_error = 0;
+error_inList = 0;
+error_cnnList = 0;
 
 for i = 1:val_im
         
          instanceWeights = imdb.images.labels(:,:,1,i) ~= 0 ;
          
-         t = ( imdb.images.data(:,:,4,i)- imdb.images.labels(:,:,1,i)) .^ 2 ;
+         t = (80* imdb.images.data(:,:,4,i)- imdb.images.labels(:,:,1,i)) .^ 2 ;
          y = instanceWeights .*t;
          y = sum(y);
-         error_ = y/sum(sum(instanceWeights));
-         error_ = sqrt(error_);
-         
+         error_in = y/sum(sum(instanceWeights));
+         error_in = sqrt(sum(error_in));
+         error_inList = [error_inList error_in];
         
          net.eval({'images', imdb.images.data(:,:,:,i), 'labels', single(imdb.images.labels(:,:,1,i))},'test');
          cnn_out = net.getValue('prediction');
-         
-         
-         t = ( 80* imdb.images.data(:,:,4,i)- cnn_out) .^ 2 ;
-         y = instanceWeights .*t;
-         y = sum(sum(y));
-        error_ = y/288*1280;
-        error_ = sqrt(error_);
-        
-        error = error + net.getValue('loss1'); 
+         error_cnn = net.getValue('loss1'); 
+         error_cnnList = [error_cnnList error_cnn]; 
 end 
 
-ave_error = [ave_error error/val_im];
-error = 0;
-
- t = (80*( imdb.images.data(:,:,4,i)- cnn_out) .^ 2) ;
- y = sum(sum(t));
-error_ = y/288*1280;
-error_ = sqrt(error_);
+save('D:\convv\results\inout_RMSE.mat','error_cnnList','error_inList');
