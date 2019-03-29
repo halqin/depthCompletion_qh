630e9edcbf8f5a64433ea64d5e0f59c3aa10ca17
halqin <gavin.qinhao@gmail.com>
2019-03-29 20:22:41 +0100

    inout_rmse

--- input_output_rmse.m
+++ input_output_rmse.m
@@ -4,24 +4,30 @@ clear all;
 [input_name, model_name]= input_output_path();
 load(model_name);
 load(input_name);
-% load('D:\convv\Git\data\morph_test100.mat') %load the input
-% load('D:\convv\Git\model\morp\net-epoch-200-morp'); %load pretrained model 
-% load('D:\convnet\depthCompletionNet-master\data\morph_test100.mat') %load the input
-% load('D:\convnet\model_result\models\demo_morp\net-epoch-200'); %load pretrained model 
+
 net = Net(net);
 size_ = size(imdb.images.data);
 % val_im = size_(4);
 val_im = 3;
-imdb.images.data(:,:,4,:) = imdb.images.data(:,:,4,:)/80;
+% imdb.images.data(:,:,4,:) = imdb.images.data(:,:,4,:)/80;
 imdb.images.data(:,:,1:3,:) = imdb.images.data(:,:,1:3,:)/255;
 error =0; 
 ave_error = 0;
 
 for i = 1:val_im
+        
+         instanceWeights = imdb.images.labels(:,:,1,i) ~= 0 ;
+         
+         t = ( imdb.images.data(:,:,4,i)- imdb.images.labels(:,:,1,i)) .^ 2 ;
+         y = instanceWeights .*t;
+         y = sum(y);
+         error_ = y/sum(sum(instanceWeights));
+         error_ = sqrt(error_);
+         
+        
          net.eval({'images', imdb.images.data(:,:,:,i), 'labels', single(imdb.images.labels(:,:,1,i))},'test');
          cnn_out = net.getValue('prediction');
          
-         instanceWeights = imdb.images.data(:,:,4,i) ~= 0 ;
          
          t = ( 80* imdb.images.data(:,:,4,i)- cnn_out) .^ 2 ;
          y = instanceWeights .*t;
@@ -31,8 +37,9 @@ for i = 1:val_im
         
         error = error + net.getValue('loss1'); 
 end 
-     ave_error = [ave_error error/val_im];
-     error = 0;
+
+ave_error = [ave_error error/val_im];
+error = 0;
 
  t = (80*( imdb.images.data(:,:,4,i)- cnn_out) .^ 2) ;
  y = sum(sum(t));
