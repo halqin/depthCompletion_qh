dde6feb815518067b81c89e2bcfb9b32af3501fe
Hao Qin <hao.qin@ugent.be>
2019-07-08 16:05:20 +0200

    layermse

--- +evalmodel/cnnOuterror.m
+++ +evalmodel/cnnOuterror.m
@@ -1,7 +1,8 @@
-function [error_cnn, cnn_out]= cnnOuterror(input_data, input_labels, net)
+function [error_cnn, cnn_out]= cnnOuterror(input_data, input_labels, net, outlayer)
 % input_data RGB-D data
 % input_labels: ground truth data
 % net: the net object 
+% outlayer: the name of outlayer
     if net.gpu
              net.eval({'images',gpuArray(single( input_data)), 'labels', gpuArray(single(input_labels))},'test');
     else
@@ -11,6 +12,6 @@ function [error_cnn, cnn_out]= cnnOuterror(input_data, input_labels, net)
     try
         cnn_out = gather(net.getValue('prediction'));
     catch  
-        cnn_out = gather(net.getValue('output'));
+        cnn_out = gather(net.getValue(outlayer));
     end 
 end
--- layer_rmse.m
+++ layer_rmse.m
@@ -1,14 +1,11 @@
-function [error_cnn, cnn_out]= layer_rmse(input_data, input_labels, net)
+% function [error_cnn, cnn_out]= layer_rmse(input_data, input_labels, net, )
 % this function is used for checking the rmse in layers 
 setup_autonn;
 vl_setupnn;
+num_im = 2;
+[input_name, model_name] = layer_rmse_path();
 
-model1 = load('f:\convnet\data\sparse_org\imdb_sparse_500.mat');
-% ani = load( 'F:\convnet\data\sparse_org\imdb_sparse_500.mat');
-for i
-
-
+error = evalmodel.evalModel(input_name, model_name, num_im, 'mse', 'output');
 
 
-
-end 
+% end 
